9c5b78bece22ee0be9f562f286334199b118390d
harsh kochar <harshkochar88@gmail.com>
2025-07-19 18:23:45 +0530

    #Deploy

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,18 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import notesRouter from "./routes/notes.js";
 import dotenv from "dotenv";
+import serverless from "serverless-http";
+
 dotenv.config();
 
-
 const app = express();
-const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/api/notes", notesRouter);
+app.use('/api/notes', notesRouter);
+
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch(err => console.error(err)); 
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+export const handler = serverless(app);
